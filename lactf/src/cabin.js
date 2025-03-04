ecb8c8dabd1a509279fc9f6cf069383f889782cc
cheonhy25u <kor01chs@gmail.com>
2025-03-04 14:45:11 +0900

    0304 add

---
+++ lactf/src/cabin.js
@@ -0,0 +1,24 @@
+const msg = document.getElementById("msg");
+const flagInp = document.getElementById("flag");
+const checkBtn = document.getElementById("check");
+
+function checkFlag(flag) {
+    const step1 = btoa(flag);
+    const step2 = step1.split("").reverse().join("");
+    const step3 = step2.replaceAll("Z", "[OLD_DATA]");
+    const step4 = encodeURIComponent(step3);
+    const step5 = btoa(step4);
+    return step5 === "JTNEJTNEUVgwNVclNUJPTERfREFUQSU1RHRWR2JGUlhkdzVXU00xRVZJQkNkalZtYWk5Mlc=";
+}
+
+checkBtn.addEventListener("click", () => {
+    const flag = flagInp.value.toLowerCase();
+    if (checkFlag(flag)) {
+        flagInp.remove();
+        checkBtn.remove();
+        msg.innerText = flag;
+        msg.classList.add("correct");
+    } else {
+        msg.classList.add("")
+    }
+});
